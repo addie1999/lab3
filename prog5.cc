8d7262f4288441859522b915d08fcd5edcde6a64
addie1999 <112737772+addie1999@users.noreply.github.com>
2022-09-16 23:14:02 -0400

    prog5.cc updated to accurately perform calculations

--- prog5.cc
+++ prog5.cc
@@ -1,5 +1,12 @@
+/*
+    Name: Addie Duffy
+    Lab #: [FILL IN HERE]
+    Description: prog5 for lab3, CONVERTING CELCIUS TO FAHRENHEIT & vice versa
+*/
+
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -7,14 +14,14 @@ int main()
    double ctof;  // equivalent Celsius temperature
    double ftoc;  // equivalent Fahrenheit temperature.
  
-   int fah = 56;  //declare and initialize at the same time - page 48
-   int cel = 20;
+   double fah = 56.0;  //declare and initialize at the same time - page 48
+   double cel = 20.0;
 
-   ctof = 9/5  * cel + 32;
-   ftoc = 5/9 * (fah -32);
+   ctof = ((9.0/5.0) * cel) + 32.0;
+   ftoc = (5.0/9.0) * (fah - 32.0);
 
-   cout << cel << " degrees Celsius in Fahrenheit is " << ctof << endl;
-   cout << fah << " degrees Fahrenheit in Celsius is " << ftoc << endl;
+   cout << cel << " degrees Celsius in Fahrenheit is " << ctof << setprecision(3) << endl;
+   cout << fah << " degrees Fahrenheit in Celsius is " << ftoc << setprecision(3) << endl;
 
    return (EXIT_SUCCESS);
 }
