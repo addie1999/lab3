041592cfde736d8b70b9b0616b31c29b1a735cab
addie1999 <112737772+addie1999@users.noreply.github.com>
2022-09-16 22:35:33 -0400

    reformatted prog1.cc for readability

--- prog1.cc
+++ prog1.cc
@@ -1,13 +1,27 @@
-// Don't forget to include the header block
-// more information on keyword const on pages 95 - 97
-// For if else format look at page 77
+/*
+    Name: Addie Duffy
+    Lab #: [FILL IN HERE]
+    Description: prog1 for lab3
+*/
 
-#include<iostream> 
-#include<cstdlib>
+#include <iostream> 
+#include <cstdlib>
+
 using namespace std;
-const int INT1 = 15;const int INT2 = 20; int main()
-{cout<<"Sum of"<<INT1 <<"and"<<INT2<<"is"<<INT1+INT2;cout
-<<"Product is"<<INT1*INT2<<endl;
-if(INT1<INT2) cout<<INT2 <<"is bigger";
-else cout<<INT1<<"is bigger"; return (EXIT_SUCCESS);
+
+const int INT1 = 15;
+const int INT2 = 20;
+
+int main(){
+    cout << "Sum of " << INT1 << " and " << INT2 << " is " << INT1 + INT2 << endl;
+    cout << "Product is " << INT1 * INT2 << endl;
+    
+    if (INT1 < INT2){
+        cout << INT2 << " is bigger" << endl;
+    }
+    else{
+        cout << INT1 << " is bigger" << endl;
+    }
+
+    return (EXIT_SUCCESS);
 }
